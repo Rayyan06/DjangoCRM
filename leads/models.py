6440f7eff5d0c1edaa0cf0b91a9380dbb0dc1dc8
Rayyan Khan <weatherwhiz06@gmail.com>
2023-11-20 08:16:10 -0500

    Modifying models.py

--- leads/models.py
+++ leads/models.py
@@ -3,6 +3,15 @@ from django.db import models
 
 
 class Lead(models.Model):
+
+    SOURCE_CHOICES = (
+        
+    )
+
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
+
+    phoned = models.BooleanField(default=False)
+    source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
+
