4b1653cb0c0cc7feee76586edac288e8cc9e36b9
Rayyan Khan <weatherwhiz06@gmail.com>
2023-11-20 11:59:52 -0500

    Created Lead List Page

--- leads/views.py
+++ leads/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Lead
 
-def home_page(request):
+def lead_list(request):
     leads = Lead.objects.all()
-    context = {"leads": leads}
-    return render(request, "second_page.html", context)
+    context = {
+        "leads": leads
+    }
+
+    return render(request, "leads/lead_list.html", context)
